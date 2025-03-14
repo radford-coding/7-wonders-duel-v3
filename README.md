2e69bfee5911e5f08559cbd2266252daa9cad8ab
Matt Radford <quinquex@gmail.com>
2025-03-14 15:45:52 -0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# 7 wonders duel
+
+Programming project for my own understanding of React. Not for commercial use.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+
+  return (
+    <>
+      <NavBar />
+      <h1>Hello, friend!</h1>
+    </>
+  );
+};
+
+export default App;
