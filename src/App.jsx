1d8eaa3f80785827e27e5454e5e10274b2cdaadc
Matt Radford <quinquex@gmail.com>
2025-03-17 07:17:20 -0700

    refactored with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useReducer } from "react";
+import { useReducer } from "react";
 import * as data from "./data";
 import './App.css';
 import Conflict from "./components/Conflict/Conflict";
@@ -6,67 +6,18 @@ import { cardReducer, initialState } from "./services/reducerHooks";
 
 const App = () => {
 
-  const [state, dispatch] = useReducer(cardReducer, {...initialState, boardCards: data.cardsAge1});
-
-  // const [isPlayerTurn, setIsPlayerTurn] = useState(true);
-  // const [playerCards, setPlayerCards] = useState([]);
-  // const [aiCards, setAiCards] = useState([]);
-  // const [boardCards, setBoardCards] = useState([]);
-
-
-
-  // const [discards, setDiscards] = useState([]);
-  // const [coins, setCoins] = useState({
-  //   player: 7,
-  //   ai: 7,
-  // });
-  // start at index 9 (space #10 of 19)
-  // const [military, setMilitary] = useState(9);
-
   // start with age 1 cards
-  // useEffect(() => {
-  //   setBoardCards(data.cardsAge1);
-  // }, []);
-
-  // const handleBuy = (card) => {
-  //   dispatch({ type: 'buyCard', card: card });
-  //   if (isPlayerTurn) {
-  //     setPlayerCards([...state.playerCards, card]);
-  //     if (card.costs.gold) setCoins({ ...coins, player: coins.player - card.costs.gold });
-  //     if (card.produces.shields) setMilitary(military + card.produces.shields);
-  //     setIsPlayerTurn(!isPlayerTurn);
-
-  //   } else {
-  //     setAiCards([...aiCards, card]);
-  //     if (card.costs.gold) setCoins({ ...coins, ai: coins.ai - card.costs.gold });
-  //     if (card.produces.shields) setMilitary(military - card.produces.shields);
-  //     setIsPlayerTurn(!isPlayerTurn);
-  //   };
-  //   setBoardCards(boardCards.filter(c => c !== card));
-  // };
+  const [state, dispatch] = useReducer(cardReducer, { ...initialState, boardCards: data.cardsAge1 });
 
   const handleBuy = (card) => {
     dispatch({ type: 'buyCard', card });
   };
 
-  // const handleSpend = (card, who) => {
+  const handleSell = (card) => {
+    console.log(+state.isPlayerTurn * 2 - 1);
+    dispatch({ type: 'sellCard', card });
+  };
 
-  // };
-
-  // const handleSell = (card) => {
-  //   if (isPlayerTurn) {
-  //     const numOfYellows = playerCards.filter(card => card.type === 'commerce').length;
-  //     setDiscards([...discards, card]);
-  //     setCoins({ ...coins, player: coins.player + 2 + numOfYellows });
-  //     setIsPlayerTurn(!isPlayerTurn);
-  //   } else {
-  //     const numOfYellows = aiCards.filter(card => card.type === 'commerce').length;
-  //     setDiscards([...discards, card]);
-  //     setCoins({ ...coins, ai: coins.ai + 2 + numOfYellows });
-  //     setIsPlayerTurn(!isPlayerTurn);
-  //   };
-  //   setBoardCards(boardCards.filter(c => c !== card));
-  // };
 
   // const calculate = (cardArray) => {
   //   let productionObj = {};
@@ -90,7 +41,7 @@ const App = () => {
   return (
     <>
       <h1>7 Wonders Duel</h1>
-      {/* <Conflict military={military}></Conflict> */}
+      <Conflict military={state.militaryConflict}></Conflict>
       <p>turn: {state.isPlayerTurn ? 'player' : 'AI'}</p>
       <div className="container">
         <div>
@@ -112,15 +63,14 @@ const App = () => {
                 {JSON.stringify(card.costs)}
                 <br />
                 <button onClick={() => handleBuy(card)}>buy</button>
-                {/* <button onClick={() => handleSell(card)}>sell</button> */}
-                <button>sell</button>
+                <button onClick={() => handleSell(card)}>sell</button>
                 <button>wonder</button>
               </li>
             ))}
           </ul>
         </div>
         <div>
-          {/* <h2>player coins: {coins.player}</h2> */}
+          <h2>player coins: {state.coins.player}</h2>
           <h2>player cards:</h2>
           <div>
             {/* {Object.keys(calculate(playerCards)).map((k, i) => <p key={i}>{k}: {calculate(playerCards)[k]}</p>)} */}
@@ -139,7 +89,7 @@ const App = () => {
           </ul>
         </div>
         <div>
-          {/* <h2>AI coins: {coins.ai}</h2> */}
+          <h2>AI coins: {state.coins.ai}</h2>
           <h2>AI cards:</h2>
           <div>
             {/* {Object.keys(calculate(aiCards)).map((k, i) => <p key={i}>{k}: {calculate(aiCards)[k]}</p>)} */}
--- src/services/reducerHooks.jsx
+++ src/services/reducerHooks.jsx
@@ -1,31 +1,48 @@
 const initialState = {
     boardCards: [],
+    discards: [],
     playerCards: [],
     aiCards: [],
     isPlayerTurn: true,
+    coins: {
+        player: 7,
+        ai: 7,
+    },
+    militaryConflict: 9,
 };
 
 const cardReducer = (state, action) => {
-
     if (action.type === 'buyCard') {
         const updatedState = {
             ...state,
             playerCards: state.isPlayerTurn ? [...state.playerCards, action.card] : state.playerCards,
             aiCards: !state.isPlayerTurn ? [...state.aiCards, action.card] : state.aiCards,
             boardCards: state.boardCards.filter(c => c !== action.card),
+            coins: {
+                ...state.coins,
+                player: (action.card.costs.gold && state.isPlayerTurn) ? state.coins.player - action.card.costs.gold : state.coins.player,
+                ai: (action.card.costs.gold && !state.isPlayerTurn) ? state.coins.ai - action.card.costs.gold : state.coins.ai,
+            },
+            militaryConflict: action.card.produces.shields ? state.militaryConflict + (+state.isPlayerTurn * 2 - 1) * action.card.produces.shields : state.militaryConflict,
             isPlayerTurn: !state.isPlayerTurn,
         };
         return updatedState;
-        // setPlayerCards([...state.playerCards, card]);
-        // if (card.costs.gold) setCoins({ ...coins, player: coins.player - card.costs.gold });
-        // if (card.produces.shields) setMilitary(military + card.produces.shields);
-        // setIsPlayerTurn(!isPlayerTurn);
+    } else if (action.type === 'sellCard') {
+        const updatedState = {
+            ...state,
+            boardCards: state.boardCards.filter(c => c !== action.card),
+            discards: [...state.discards, action.card],
+            coins: {
+                ...state.coins,
+                player: state.isPlayerTurn ? state.coins.player + 2 + state.playerCards.filter(c => c.type === 'commerce').length : state.coins.player,
+                ai: !state.isPlayerTurn ? state.coins.ai + 2 + state.aiCards.filter(c => c.type === 'commerce').length : state.coins.ai,
+            },
+            isPlayerTurn: !state.isPlayerTurn,
+        };
+        return updatedState;
     };
     // catchall if we messed up
     return state
-
-
-
 };
 
 export { cardReducer, initialState };
